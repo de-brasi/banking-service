097f8979876abaa4a80edf74571846fb5604a2c9
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-23 20:20:06 +0300

    initial commit

---
+++ src/main/java/my/solution/dto/RegisterClientRequest.java
@@ -0,0 +1,40 @@
+package my.solution.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import my.solution.dto.validators.PhoneOrEmailMustBeProvided;
+
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@PhoneOrEmailMustBeProvided
+public class RegisterClientRequest {
+    @NotBlank(message = "The login is required.")
+    String login;
+
+    @NotBlank(message = "The password is required.")
+    String password;    // todo: по-моему так не стоит делать
+
+    @NotBlank(message = "The first name is required.")
+    @JsonProperty("first_name")
+    String firstName;
+
+    @NotBlank(message = "The last name is required.")
+    @JsonProperty("last_name")
+    String lastName;
+
+    String patronymic;
+
+    String phone;
+    String email;
+
+    @NotNull(message = "The birth date is required.")
+    @JsonProperty("birth_date")
+    OffsetDateTime birthDate;
+}
