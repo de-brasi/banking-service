7729b1e1ec66e2864dd8011e1f92f3d0a3568ab3
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-23 21:12:55 +0300

    better structure

--- migrations/init-tables.sql
+++ migrations/init-tables.sql
@@ -1,18 +1,20 @@
-CREATE TABLE IF NOT EXISTS clients (
-    client_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    login TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    phone_number TEXT UNIQUE,
-    email_address TEXT UNIQUE,
-    birth_date DATE NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    patronymic TEXT
+CREATE TABLE IF NOT EXISTS bank_accounts
+(
+    account_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    deposit    NUMERIC(25, 2) DEFAULT 0 NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank_accounts (
-    account_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    client BIGINT UNIQUE NOT NULL,
-    deposit NUMERIC(25, 2) DEFAULT 0,
-    FOREIGN KEY (client) REFERENCES clients(client_id)
+CREATE TABLE IF NOT EXISTS clients
+(
+    client_id     BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    login         TEXT          NOT NULL UNIQUE,
+    password      TEXT          NOT NULL,
+    account       BIGINT UNIQUE NOT NULL,
+    phone_number  TEXT UNIQUE,
+    email_address TEXT UNIQUE,
+    birth_date    DATE          NOT NULL,
+    first_name    TEXT          NOT NULL,
+    last_name     TEXT          NOT NULL,
+    patronymic    TEXT,
+    FOREIGN KEY (account) REFERENCES bank_accounts (account_id)
 );
