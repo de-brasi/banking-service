91714ad877f4d42a39f33be463305d7f3da023c5
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-24 16:07:47 +0300

    add EnableConfigurationProperties annotation

--- src/main/java/my/solution/BankApplication.java
+++ src/main/java/my/solution/BankApplication.java
@@ -1,11 +1,12 @@
 package my.solution;
 
+import my.solution.configuration.BankServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties(BankServiceConfig.class)
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
