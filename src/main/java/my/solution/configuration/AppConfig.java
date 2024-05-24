d2b6c32215a912de4b7ccf6405b081b096ca6f3b
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-25 02:19:08 +0300

    init

---
+++ src/main/java/my/solution/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package my.solution.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class AppConfig {
+}
