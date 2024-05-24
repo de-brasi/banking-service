af06b77c1a11a4e326f7d3fa05916a04087c9cdb
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-24 15:15:37 +0300

    add search-service usage

--- src/main/java/my/solution/api/ServiceApiController.java
+++ src/main/java/my/solution/api/ServiceApiController.java
@@ -3,6 +3,7 @@ package my.solution.api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import my.solution.dto.RegisterClientRequest;
+import my.solution.dto.SearchClientsRequest;
 import my.solution.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +25,10 @@ public class ServiceApiController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    // todo: собственно сам поисковый сервис
     @GetMapping(value = "/search")
-    public ResponseEntity<?> handleSearch() {
+    public ResponseEntity<?> handleSearch(@Validated @RequestBody SearchClientsRequest requestBody) {
+        log.info("Got search request: {}", requestBody);
+        clientService.searchClients(requestBody);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
