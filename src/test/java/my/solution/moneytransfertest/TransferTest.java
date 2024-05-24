0a37a990fcb2b7c3ca4eb227295abd727a959503
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-25 02:58:18 +0300

    init

---
+++ src/test/java/my/solution/moneytransfertest/TransferTest.java
@@ -0,0 +1,317 @@
+package my.solution.moneytransfertest;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import my.solution.repository.ClientRepository;
+import my.solution.repository.entity.Client;
+import my.solution.service.DepositService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class TransferTest extends IntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @MockBean
+    private DepositService depositService;
+
+    private RestClient ADMIN;
+
+    @Test
+    public void dbConnectionTest() {
+        final String jdbcUrl = POSTGRES.getJdbcUrl();
+        final String username = POSTGRES.getUsername();
+        final String password = POSTGRES.getPassword();
+
+        try (java.sql.Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            assertThat(connection.getCatalog()).isEqualTo("bank_db");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void simpleMoneyTransactionTest() {
+        ADMIN = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+
+        // client 1
+        RestClient client1 = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+        final String loginInfoClient1 = """
+                {
+                  "login": "firstClient",
+                  "password": "firstClientPassword",
+                  "patronymic": null,
+                  "phone": "phone1",
+                  "email": "email1",
+                  "initial_deposit": 100,
+                  "first_name": "first name",
+                  "last_name": "last name",
+                  "birth_date": "2024-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // client 2
+        final String loginInfoClient2 = """
+                {
+                  "login": "secondClient",
+                  "password": "secondClientPassword",
+                  "patronymic": null,
+                  "phone": "phone2",
+                  "email": "email2",
+                  "initial_deposit": 0,
+                  "first_name": "second name",
+                  "last_name": "second name",
+                  "birth_date": "2025-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // register clients
+        var firstRegistrationResult = registerClient(ADMIN, loginInfoClient1);
+        var secondRegistrationResult = registerClient(ADMIN, loginInfoClient2);
+
+        assertThat(firstRegistrationResult).isEqualTo(HttpStatus.OK);
+        assertThat(secondRegistrationResult).isEqualTo(HttpStatus.OK);
+
+        // transfer
+        var transferResult = client1.patch()
+                .uri("/client/firstClient/transfer-money")
+                .header("Receiver-Login", "secondClient")
+                .header("Password", "firstClientPassword")
+                .header("Amount", "100")
+                .retrieve()
+                .toBodilessEntity()
+                .getStatusCode();
+
+        assertThat(transferResult).isEqualTo(HttpStatus.OK);
+
+        Client sender = clientRepository.findClientByLogin("firstClient").orElseThrow();
+        Client receiver = clientRepository.findClientByLogin("secondClient").orElseThrow();
+        assertThat(sender.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(0).setScale(2, RoundingMode.HALF_EVEN));
+        assertThat(receiver.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(100.00).setScale(2, RoundingMode.HALF_EVEN));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void concurrentReadWriteTransactionTest() throws InterruptedException {
+        ADMIN = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+
+        // sender 1
+        RestClient sender = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+        final String loginInfoClient = """
+                {
+                  "login": "sender1",
+                  "password": "sender1",
+                  "patronymic": null,
+                  "phone": "phone11",
+                  "email": "email11",
+                  "initial_deposit": 100,
+                  "first_name": "first name",
+                  "last_name": "last name",
+                  "birth_date": "2024-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // receiver
+        final String receiver = """
+                {
+                  "login": "receiver",
+                  "password": "receiver",
+                  "patronymic": null,
+                  "phone": "phone33",
+                  "email": "email33",
+                  "initial_deposit": 0,
+                  "first_name": "third name",
+                  "last_name": "third name",
+                  "birth_date": "2025-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // register clients
+        var sender1RegistrationResult = registerClient(ADMIN, loginInfoClient);
+        var luckyOneRegistrationResult = registerClient(ADMIN, receiver);
+
+
+        assertThat(sender1RegistrationResult).isEqualTo(HttpStatus.OK);
+        assertThat(luckyOneRegistrationResult).isEqualTo(HttpStatus.OK);
+
+        // concurrent transfer
+        var worker1 = new Thread(new TransferRoutine(
+                sender, 1, 50,
+                "sender1", "sender1", "receiver"));
+        var worker2 = new Thread(new TransferRoutine(
+                sender, 1, 50,
+                "sender1", "sender1", "receiver"));
+
+        worker1.start();
+        worker2.start();
+        // await transfer
+        worker1.join();
+        worker2.join();
+
+        Client sender1Client = clientRepository.findClientByLogin("sender1").orElseThrow();
+        Client receiverClient = clientRepository.findClientByLogin("receiver").orElseThrow();
+
+        System.out.println(receiverClient.getAccount().getDeposit());
+        System.out.println(sender1Client.getAccount().getDeposit());
+
+        assertThat(receiverClient.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_EVEN));
+        assertThat(sender1Client.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(0).setScale(2, RoundingMode.HALF_EVEN));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void concurrentTransactionTest() throws InterruptedException {
+        ADMIN = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+
+        // sender 1
+        RestClient sender1 = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+        final String loginInfoClient1 = """
+                {
+                  "login": "sender111",
+                  "password": "sender111",
+                  "patronymic": null,
+                  "phone": "phone111",
+                  "email": "email111",
+                  "initial_deposit": 100,
+                  "first_name": "first name",
+                  "last_name": "last name",
+                  "birth_date": "2024-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // sender 2
+        RestClient sender2 = RestClient.builder().baseUrl("http://localhost:%d".formatted(port)).build();
+        final String loginInfoClient2 = """
+                {
+                  "login": "sender222",
+                  "password": "sender222",
+                  "patronymic": null,
+                  "phone": "phone222",
+                  "email": "email222",
+                  "initial_deposit": 100,
+                  "first_name": "second name",
+                  "last_name": "second name",
+                  "birth_date": "2025-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // receiver
+        final String luckyOneInfo = """
+                {
+                  "login": "receiver1",
+                  "password": "receiver1",
+                  "patronymic": null,
+                  "phone": "phone333",
+                  "email": "email333",
+                  "initial_deposit": 0,
+                  "first_name": "third name",
+                  "last_name": "third name",
+                  "birth_date": "2025-05-24T14:27:46.929Z"
+                }
+                """;
+
+        // register clients
+        var sender1RegistrationResult = registerClient(ADMIN, loginInfoClient1);
+        var sender2RegistrationResult = registerClient(ADMIN, loginInfoClient2);
+        var luckyOneRegistrationResult = registerClient(ADMIN, luckyOneInfo);
+
+
+        assertThat(sender1RegistrationResult).isEqualTo(HttpStatus.OK);
+        assertThat(sender2RegistrationResult).isEqualTo(HttpStatus.OK);
+        assertThat(luckyOneRegistrationResult).isEqualTo(HttpStatus.OK);
+
+        // concurrent transfer
+        var worker1 = new Thread(new TransferRoutine(
+                sender1, 1, 100,
+                "sender111", "sender111", "receiver1"));
+        var worker2 = new Thread(new TransferRoutine(
+                sender2, 1, 100,
+                "sender222", "sender222", "receiver1"));
+
+        worker1.start();
+        worker2.start();
+
+        // await transfers
+        worker1.join();
+        worker2.join();
+
+        Client sender1Client = clientRepository.findClientByLogin("sender111").orElseThrow();
+        Client sender2Client = clientRepository.findClientByLogin("sender222").orElseThrow();
+        Client receiver = clientRepository.findClientByLogin("receiver1").orElseThrow();
+
+        System.out.println(receiver.getAccount().getDeposit());
+        System.out.println(sender1Client.getAccount().getDeposit());
+        System.out.println(sender2Client.getAccount().getDeposit());
+
+        assertThat(receiver.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(200).setScale(2, RoundingMode.HALF_EVEN));
+        assertThat(sender1Client.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(0).setScale(2, RoundingMode.HALF_EVEN));
+        assertThat(sender2Client.getAccount().getDeposit())
+                .isEqualTo(BigDecimal.valueOf(0).setScale(2, RoundingMode.HALF_EVEN));
+    }
+
+    private record TransferRoutine(RestClient sender, int amount, int iterationCount,
+                                   String senderLogin,
+                                   String senderPassword,
+                                   String receiverLogin) implements Runnable {
+        @Override
+        public void run() {
+            int success = 0;
+            for (int i = 0; i < iterationCount; i++) {
+                var res = sender.patch()
+                        .uri("/client/%s/transfer-money".formatted(senderLogin()))
+                        .header("Receiver-Login", receiverLogin())
+                        .header("Password", senderPassword())
+                        .header("Amount", String.valueOf(amount))
+                        .retrieve()
+                        .toBodilessEntity()
+                        .getStatusCode();
+                if (res.is2xxSuccessful()) {
+                    success++;
+                }
+            }
+            log.error("Success rate: {}/{}", success, iterationCount);
+        }
+    }
+
+    private static HttpStatusCode registerClient(RestClient register, String body) {
+        return register
+                .post()
+                .uri("/api/admin/register")
+                .contentType(APPLICATION_JSON)
+                .body(body)
+                .retrieve()
+                .toBodilessEntity()
+                .getStatusCode();
+    }
+
+}
