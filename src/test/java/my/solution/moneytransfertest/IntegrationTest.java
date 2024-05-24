a7d5e3759acc1f9e390ec5f2ee8255f74170d2b8
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-24 16:40:19 +0300

    init

---
+++ src/test/java/my/solution/moneytransfertest/IntegrationTest.java
@@ -0,0 +1,70 @@
+package my.solution.moneytransfertest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class IntegrationTest {
+    public static PostgreSQLContainer<?> POSTGRES;
+
+    static {
+        POSTGRES = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("bank_db")
+                .withUsername("postgres")
+                .withPassword("postgres");
+        POSTGRES.start();
+
+        runMigrations(POSTGRES);
+    }
+
+    private static void runMigrations(JdbcDatabaseContainer<?> c) {
+        final String jdbcUrl = c.getJdbcUrl();
+        final String username = c.getUsername();
+        final String password = c.getPassword();
+
+        final String changelogFilePath = new File(".")
+                .toPath().toAbsolutePath()
+                .getParent().getParent()
+                .resolve("migrations").resolve("master.xml")
+                .toString();
+
+        try (java.sql.Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            Database database = DatabaseFactory
+                    .getInstance()
+                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new liquibase.Liquibase("master.xml",
+                    new DirectoryResourceAccessor(new File(".")
+                            .toPath().toAbsolutePath()
+                            .getParent()
+                            .resolve("migrations")),
+                    database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @DynamicPropertySource
+    static void jdbcProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
