a747ad3f27496e08ad96aa63455cc66d0d86e50f
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-24 15:14:45 +0300

    remove

--- src/main/java/my/solution/api/exceptions/ContactValueAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package my.solution.api.exceptions;
-
-public class ContactValueAlreadyExistsException extends RuntimeException {
-    public ContactValueAlreadyExistsException(String contactValue) {
-        super("Contact value of %s already belong to another client!".formatted(contactValue));
-    }
-}
