ac13e7b535d3b40aca909d06f3a65aedc04d6c7d
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-25 02:58:48 +0300

    add SERIALIZABLE isolation

--- src/main/java/my/solution/repository/BankAccountRepository.java
+++ src/main/java/my/solution/repository/BankAccountRepository.java
@@ -1,9 +1,16 @@
 package my.solution.repository;
 
 import my.solution.repository.entity.BankAccount;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    default void saveWithSerializable(@NotNull BankAccount entity) {
+        save(entity);
+    }
 }
