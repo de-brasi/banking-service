6a50b530e2eb0d95d3f8c0cb8d40d3704ae08664
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-24 15:17:03 +0300

    add handlers for ClientNotExistsException and NotEnoughMoneyException exceptions

--- src/main/java/my/solution/api/ExceptionController.java
+++ src/main/java/my/solution/api/ExceptionController.java
@@ -1,8 +1,6 @@
 package my.solution.api;
 
-import my.solution.api.exceptions.ContactValueAlreadyExistsException;
-import my.solution.api.exceptions.InsufficientFundsException;
-import my.solution.api.exceptions.LastContactRemoveException;
+import my.solution.api.exceptions.*;
 import my.solution.dto.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,6 +42,36 @@ public class ExceptionController {
         return new ResponseEntity<>(apiErrorResponse, httpResponseCode);
     }
 
+    @ExceptionHandler(ClientNotExistsException.class)
+    public ResponseEntity<ApiErrorResponse> invalidClientException(Exception e) {
+        final var httpResponseCode = HttpStatus.BAD_REQUEST;
+        final var apiErrorResponse = new ApiErrorResponse(
+                "No client with such id.",
+                String.valueOf(httpResponseCode.value()),
+                e.getClass().getCanonicalName(),
+                e.getMessage(),
+                Arrays.stream(e.getStackTrace())
+                        .map(StackTraceElement::toString)
+                        .toList()
+        );
+        return new ResponseEntity<>(apiErrorResponse, httpResponseCode);
+    }
+
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ApiErrorResponse> notEnoughMoneyException(Exception e) {
+        final var httpResponseCode = HttpStatus.BAD_REQUEST;
+        final var apiErrorResponse = new ApiErrorResponse(
+                "Not enough money.",
+                String.valueOf(httpResponseCode.value()),
+                e.getClass().getCanonicalName(),
+                e.getMessage(),
+                Arrays.stream(e.getStackTrace())
+                        .map(StackTraceElement::toString)
+                        .toList()
+        );
+        return new ResponseEntity<>(apiErrorResponse, httpResponseCode);
+    }
+
     @ExceptionHandler(InsufficientFundsException.class)
     public ResponseEntity<ApiErrorResponse> insufficientFundsExceptionHandler(Exception e) {
         final var httpResponseCode = HttpStatus.BAD_REQUEST;
@@ -59,11 +87,11 @@ public class ExceptionController {
         return new ResponseEntity<>(apiErrorResponse, httpResponseCode);
     }
 
-    @ExceptionHandler(ContactValueAlreadyExistsException.class)
-    public ResponseEntity<ApiErrorResponse> alreadyExistsContactException(Exception e) {
+    @ExceptionHandler(ValueAlreadyExistsException.class)
+    public ResponseEntity<ApiErrorResponse> fieldAlreadyExistsException(Exception e) {
         final var httpResponseCode = HttpStatus.BAD_REQUEST;
         final var apiErrorResponse = new ApiErrorResponse(
-                "This contact is already belong to someone!",
+                "This contact contains field(email, login, phone, etc.) that is already belong to someone!",
                 String.valueOf(httpResponseCode.value()),
                 e.getClass().getCanonicalName(),
                 e.getMessage(),
