af31352bb720fcd375f68e098c35f9ea49579ff8
de-brasi <leskin.ilia.public@yandex.ru>
2024-05-25 20:28:03 +0300

    Better swagger descriptions

--- src/main/java/my/solution/dto/ApiErrorResponse.java
+++ src/main/java/my/solution/dto/ApiErrorResponse.java
@@ -1,6 +1,8 @@
 package my.solution.dto;
 
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Api error response for client")
 public class ApiErrorResponse {
     String description;
     String code;
--- src/main/java/my/solution/dto/RegisterClientRequest.java
+++ src/main/java/my/solution/dto/RegisterClientRequest.java
@@ -1,6 +1,7 @@
 package my.solution.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,30 +15,41 @@ import java.time.OffsetDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Api request to register user")
 @PhoneOrEmailMustBeProvided
 public class RegisterClientRequest {
+    @Schema(description = "New client's login", example = "BestLoginYouEverSeen228")
     @NotBlank(message = "The login is required.")
     String login;
 
+    @Schema(description = "Password for created user")
     @NotBlank(message = "The password is required.")
     String password;    // todo: по-моему так не стоит делать
 
+    @Schema(description = "Account's money value")
     @JsonProperty("initial_deposit")
     BigDecimal initialDeposit = BigDecimal.ZERO;
 
+    @Schema(description = "New client's first name", example = "Leslie")
     @NotBlank(message = "The first name is required.")
     @JsonProperty("first_name")
     String firstName;
 
+    @Schema(description = "New client's last name", example = "Nielsen")
     @NotBlank(message = "The last name is required.")
     @JsonProperty("last_name")
     String lastName;
 
+    @Schema(description = "Patronymic (if exists)")
     String patronymic;
 
+    @Schema(description = "Phone number")
     String phone;
+
+    @Schema(description = "Email address")
     String email;
 
+    @Schema(description = "Date of birth")
     @NotNull(message = "The birth date is required.")
     @JsonProperty("birth_date")
     OffsetDateTime birthDate;
